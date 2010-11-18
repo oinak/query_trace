6f8b40c1e6981b0fee44bc3e40424f900f09dceb
Fernando Martínez <fernando.mdelacueva@gmail.com>
2010-11-18 16:20:13 +0100

    Init.rb reformulation to actualy implement the database.yml activation feature devised by Jon Leighton.

--- init.rb
+++ init.rb
@@ -1,5 +1,8 @@
 require 'query_trace'
 
 class ::ActiveRecord::ConnectionAdapters::AbstractAdapter
-  include QueryTrace
+  if  ActiveRecord::Base.connection.instance_variable_get("@config")[:query_trace]
+    include QueryTrace
+  end
 end
+
